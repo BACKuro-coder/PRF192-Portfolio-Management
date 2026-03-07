a95dc787a5b2f79966c8031f4a18a5cb6e9495a6
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2026-03-07 13:34:50 +0700

    feat(validate): validate input Developer

--- PortfolioManager.h
+++ PortfolioManager.h
@@ -1,6 +1,7 @@
 #ifndef PORTFOLIOMANAGER_H
 #define PORTFOLIOMANAGER_H
 #include "DataTypes.h"
+#include <ctype.h>
 
 //manage status system
 extern Developer* devList;
@@ -15,5 +16,34 @@ void addDeveloper();
 void searchDeveloper();
 void assignProject();
 void sortDevelopersBySalary();
+
+//Check Input Format 
+int isValidDevID(const char* id){
+	if(strlen(id) != 6) return 0;//check lengh
+	if(strncmp(id, "DEV", 3) != 0) return 0;//check format
+	if(!isdigit(id[3]) || !isdigit(id[4]) || !isdigit(id[5])) return 0;// 3 char not number
+	return 1;	
+}
+
+//Check Duplicate ID
+int isDuplicateDevID(const char* id){
+	for(int i = 0; i < devCount; i++){
+		if(strcmp(devList[i].devID, id) == 0) return 1; //duplicate
+	}
+	return 0;
+}
+
+//Check min 2 wrod have space between words
+int isValidName(const char* name){
+	int len = strlen(name);
+	int spaceCount = 0;
+	
+	for(int i = 1; i < len - 1; i++){
+		if(name[i] == ' ' && name[i-1] != ' '){
+			spaceCount++;
+		}
+	}
+	return spaceCount >= 1 ? : 0;
+}
 
 #endif
