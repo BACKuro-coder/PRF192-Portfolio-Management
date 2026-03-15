f4ff548fec864e937da6bb70c0461e06b2bb29c2
BACKuro-coder <nkhoa906132@gmail.com>
2026-03-15 19:29:40 +0700

    ok

--- Dynamic Array Management (resizeArray).h
+++
@@ -1,8 +0,0 @@
-#ifndef DYNAMICARRAY_H
-#define DYNAMICARRAY_H
-
-#include "Struct Developer.h"
-
-void resizeArray();
-
-#endif
--- Menu Design.c
+++
@@ -1,9 +0,0 @@
-void displayMenu() {
-    printf("\n===== DEVELOPER MANAGEMENT =====\n");
-    printf("1. Add Developer\n");
-    printf("2. Show Developer List\n");
-    printf("3. Save Developers to File\n");
-    printf("4. Load Developers from File\n");
-    printf("0. Exit\n");
-    printf("Choose: ");
-}
