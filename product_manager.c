c64d767e5feeed40133475634dbf8ddacb113211
tuanloc-1708 <97837536+tuanloc-1708@users.noreply.github.com>
2026-03-21 12:46:33 +0700

    update finnal

--- product_manager.c
+++ product_manager.c
@@ -6,33 +6,31 @@ void initManager(ProductManager *pm, int initialCapacity) {
     pm->products = (Product *)malloc(pm->capacity * sizeof(Product));
 }
 
-int addProduct(ProductManager *pm, const char *name, double price, int quantity, const char *category) {
+int addProduct(ProductManager *pm, const char *name, const char *devID, const char *version, const char *platform) {
     if (pm->count >= pm->capacity) {
         pm->capacity *= 2;
         pm->products = (Product *)realloc(pm->products, pm->capacity * sizeof(Product));
         if (pm->products == NULL) return 0;
     }
     
-   
     int newId = pm->count + 1; 
     
     pm->products[pm->count].id = newId;
     strcpy(pm->products[pm->count].name, name);
-    pm->products[pm->count].price = price;
-    pm->products[pm->count].quantity = quantity;
-    strcpy(pm->products[pm->count].category, category);
+    strcpy(pm->products[pm->count].devID, devID);
+    strcpy(pm->products[pm->count].version, version);
+    strcpy(pm->products[pm->count].platform, platform);
     pm->products[pm->count].status = 1; 
     
     pm->count++;
     return 1; 
 }
 
-
-int updateProduct(ProductManager *pm, int searchId, double newPrice, int newQuantity) {
+int updateProduct(ProductManager *pm, int searchId, const char *newVersion, const char *newPlatform) {
     for (int i = 0; i < pm->count; i++) {
         if (pm->products[i].id == searchId && pm->products[i].status == 1) {
-            pm->products[i].price = newPrice;
-            pm->products[i].quantity = newQuantity;
+            strcpy(pm->products[i].version, newVersion);
+            strcpy(pm->products[i].platform, newPlatform);
             return 1;
         }
     }
@@ -49,26 +47,26 @@ int deleteProduct(ProductManager *pm, int searchId) {
     return 0;
 }
 
-
 void searchProduct(ProductManager *pm, const char *keyword) {
     int found = 0;
-    printf("--- Ket qua tim kiem cho '%s' ---\n", keyword);
+    printf("--- Search results for '%s' ---\n", keyword);
     for (int i = 0; i < pm->count; i++) {
-       
         if (pm->products[i].status == 1 && strstr(pm->products[i].name, keyword) != NULL) {
-            printf("ID: %d | Name: %s | Price: $%.2f | Qty: %d\n", 
-                   pm->products[i].id, pm->products[i].name, pm->products[i].price, pm->products[i].quantity);
+            printf("ID: %d | Name: %s | Dev: %s | Ver: %s | Platform: %s\n", 
+                   pm->products[i].id, pm->products[i].name, 
+                   pm->products[i].devID, pm->products[i].version,
+                   pm->products[i].platform);
             found = 1;
         }
     }
-    if (!found) printf("Khong tim thay san pham nao!\n");
+    if (!found) printf("No software product found!\n");
 }
 
-
+// Sort by name (alphabetical ascending)
 void sortProduct(ProductManager *pm) {
     for (int i = 0; i < pm->count - 1; i++) {
         for (int j = 0; j < pm->count - i - 1; j++) {
-            if (pm->products[j].price > pm->products[j+1].price) {
+            if (strcmp(pm->products[j].name, pm->products[j+1].name) > 0) {
                 Product temp = pm->products[j];
                 pm->products[j] = pm->products[j+1];
                 pm->products[j+1] = temp;
@@ -83,18 +81,21 @@ int loadProductFromFile(ProductManager *pm, const char *filename) {
 
     char line[256];
     while (fgets(line, sizeof(line), file)) {
-        char name[100];
-        double price;
-        int quantity;
+        char name[100], devID[10], version[20], platform[50];
+        
         char *token = strtok(line, ",");
         if (token != NULL) strcpy(name, token);
         
         token = strtok(NULL, ",");
-        if (token != NULL) price = atof(token);
+        if (token != NULL) strcpy(devID, token);
+        
+        token = strtok(NULL, ",");
+        if (token != NULL) strcpy(version, token);
         
         token = strtok(NULL, ",\n");
-        if (token != NULL) quantity = atoi(token);
-        addProduct(pm, name, price, quantity, "General");
+        if (token != NULL) strcpy(platform, token);
+        
+        addProduct(pm, name, devID, version, platform);
     }
     fclose(file);
     return 1;
@@ -106,8 +107,9 @@ int saveProductToFile(ProductManager *pm, const char *filename) {
 
     for (int i = 0; i < pm->count; i++) {
         if (pm->products[i].status == 1) {
-            fprintf(file, "%s,%.2f,%d\n", 
-                    pm->products[i].name, pm->products[i].price, pm->products[i].quantity);
+            fprintf(file, "%s,%s,%s,%s\n", 
+                    pm->products[i].name, pm->products[i].devID,
+                    pm->products[i].version, pm->products[i].platform);
         }
     }
     
